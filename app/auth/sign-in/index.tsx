f67950597df36da0befe064df7398f0865e6d7a8
Mastermind730 <sourav.97032@gmail.com>
2024-09-17 13:36:55 +0530

    sign in page done

--- app/auth/sign-in/index.tsx
+++ app/auth/sign-in/index.tsx
@@ -1,11 +1,17 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import React, { useEffect } from "react";
-import { useNavigation } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 import { Colors } from "@/constants/Colors";
 
 export default function Index() {
   const navigation = useNavigation();
-
+  const router = useRouter();
   useEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -17,15 +23,64 @@ export default function Index() {
       <Text style={styles.title}>Let&apos;s Sign You In</Text>
       <Text style={styles.subtitle}>Welcome Back</Text>
       <Text style={styles.subtitle}>You&apos;ve been missed</Text>
-      
+
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Email</Text>
         <TextInput
           placeholder="Enter Email"
-          placeholderTextColor={Colors.GRAY}
+          // placeholderTextColor={Colors.GRAY}
+
+          style={styles.input}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          placeholder="Enter Password"
+          secureTextEntry={true}
+          // placeholderTextColor={Colors.GRAY}
           style={styles.input}
         />
       </View>
+
+      <View
+        style={{
+          padding: 20,
+          marginTop: 50,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+        }}
+      >
+        <Text
+          style={{
+            color: Colors.WHITE,
+            textAlign: "center",
+            fontSize: 16,
+            fontFamily: "outfit-bold",
+          }}
+        >
+          Sign In
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => router.replace("/auth/sign-up")}
+        style={{
+          padding: 20,
+          marginTop: 20,
+          backgroundColor: Colors.WHITE,
+          borderRadius: 15,
+          borderWidth: 1,
+        }}
+      >
+        <Text
+          style={{
+            color: Colors.PRIMARY,
+            textAlign: "center",
+            fontSize: 16,
+            fontFamily: "outfit-medium",
+          }}
+        >
+          Create Account
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -39,33 +94,34 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   title: {
-    fontFamily: "Outfit-Bold", // Ensure the correct font family
+    fontFamily: "outfit-Bold", // Ensure the correct font family
     fontSize: 30,
     // color: Colors.BLACK,
   },
   subtitle: {
-    fontFamily: "Outfit",
+    fontFamily: "outfit",
     fontSize: 24,
     color: Colors.GRAY,
-    marginTop: 20,
+    marginTop: 10,
   },
   inputContainer: {
-    marginTop: 30,
+    marginTop: 50,
   },
   label: {
     fontFamily: "outfit",
     fontSize: 18,
     // color: Colors.BLACK,
     marginBottom: 8,
+    marginTop: 8,
   },
   input: {
     fontFamily: "outfit",
     fontSize: 16,
-    padding: 12,
+    padding: 15,
     borderWidth: 1,
     borderColor: Colors.GRAY,
-    borderRadius: 8,
-    backgroundColor: Colors.GRAY,
-    
+    borderRadius: 15,
+
+    // backgroundColor: Colors.GRAY,
   },
 });
